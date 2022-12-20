30e0ed57c23d4e67b270d6e5df8ed96b50e17a6d
Амир Байтулинов <amiricus@gmail.com>
2022-12-20 21:00:38 +0500

    Add resolutions

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    env: {
+      viewportWidth: 1440,
+      viewportHeight: 900
+    },
+    // // env: {
+    // //   viewportWidth: 375,
+    // //   viewportHeight: 667
+    // },
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
     },
--- cypress/e2e/books.cy.js
+++ cypress/e2e/books.cy.js
@@ -1,48 +1,111 @@
-it("Should open the main page", () => {
-  cy.visit('/');
-  cy.contains('Books list').should('be.visible');
-});
+describe('Books tests', () => {
+  context('HD resolution', () => {
+    beforeEach(() => {
+      cy.viewport(1366, 768)
+    })
 
-it("Should successfully login", () => {
+    it("Should open the main page", () => {
+      cy.visit('/');
+      cy.contains('Books list').should('be.visible');
+    });
+
+    it("Should successfully login", () => {
   cy.visit('/');
   cy.login('test@test.com', 'test');
   cy.contains('Добро пожаловать test@test.com').should('be.visible');
-});
+    });
 
-it("Should not login with empty email", () => {
+    it("Should not login with empty email", () => {
   cy.visit('/');
   cy.login(' ', 'test');
   cy.get('#mail')
     .then(($el) => $el[0].checkValidity())
     .should('be.false');
-});
+    });
 
-it("Should not login with empty password", () => {
+    it("Should not login with empty password", () => {
   cy.visit('/');
   cy.login('test@test.com');
   cy.get('#pass')
     .then(($el) => $el[0].checkValidity())
     .should('be.false');
-});
+    });
 
-it("Should open the book", () => {
+    it("Should open the book", () => {
   cy.visit('/');
   cy.login('test@test.com', 'test');
   cy.get('.card-body').click();
   cy.contains('Dowload book').should('be.visible');
-});
+    });
 
-it("Add the book to favorite", () => {
+    it("Add the book to favorite", () => {
   cy.visit('/');
   cy.login('test@test.com', 'test');
   cy.contains('Add to favorite').click();
   cy.contains('Delete from favorite').should('be.visible');
-});
+    });
 
-it("Delete the book from favorite", () => {
+    it("Delete the book from favorite", () => {
   cy.visit('/');
   cy.login('test@test.com', 'test');
   cy.contains('Favorites').click();
   cy.contains('Delete from favorite').click();
   cy.contains('Please add some book to favorit on home page!').should('be.visible');
-});
+    });
+  });
+
+  context('iphone-xr resolution', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-xr')
+    })
+
+    it("Should open the main page", () => {
+      cy.visit('/');
+      cy.contains('Books list').should('be.visible');
+    });
+
+    it("Should successfully login", () => {
+  cy.visit('/');
+  cy.login('test@test.com', 'test');
+  cy.contains('Добро пожаловать test@test.com').should('be.visible');
+    });
+
+    it("Should not login with empty email", () => {
+  cy.visit('/');
+  cy.login(' ', 'test');
+  cy.get('#mail')
+    .then(($el) => $el[0].checkValidity())
+    .should('be.false');
+    });
+
+    it("Should not login with empty password", () => {
+  cy.visit('/');
+  cy.login('test@test.com');
+  cy.get('#pass')
+    .then(($el) => $el[0].checkValidity())
+    .should('be.false');
+    });
+
+    it("Should open the book", () => {
+  cy.visit('/');
+  cy.login('test@test.com', 'test');
+  cy.get('.card-body').click();
+  cy.contains('Dowload book').should('be.visible');
+    });
+
+    it("Add the book to favorite", () => {
+  cy.visit('/');
+  cy.login('test@test.com', 'test');
+  cy.contains('Add to favorite').click();
+  cy.contains('Delete from favorite').should('be.visible');
+    });
+
+    it("Delete the book from favorite", () => {
+  cy.visit('/');
+  cy.login('test@test.com', 'test');
+  cy.contains('Favorites').click();
+  cy.contains('Delete from favorite').click();
+  cy.contains('Please add some book to favorit on home page!').should('be.visible');
+    });
+  });
+});    
